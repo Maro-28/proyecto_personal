77e26b581ffe9ab7bc84ef836a6521643ff2d21a
Mariano Ghislanzoni <mariano.ghislanzoni@davinci.edu.ar>
2022-02-03 19:52:31 -0300

    Creacion del proyecto

---
+++ AdminLTE-3.1.0/index.php
@@ -0,0 +1,126 @@
+<?php
+include_once('Part/head.html');
+include_once('Part/Navbar.html');
+include_once('Part/sidebar.html');
+?>
+
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Dashboard</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <!-- Left col -->
+        <section class="col-lg-6 connectedSortable">
+          <div class="card bg-gradient-info">
+            <div class="card-header border-0">
+              <h3 class="card-title">
+                <i class="fas fa-map-marker-alt mr-1"></i>
+                Visitors
+              </h3>
+              <!-- card tools -->
+              <div class="card-tools">
+                <button type="button" class="btn btn-info btn-sm" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+              </div>
+              <!-- /.card-tools -->
+            </div>
+            <div class="card-body">
+
+            </div>
+            <!-- /.card-body-->
+          </div>
+          <div class="card bg-gradient-primary">
+            <div class="card-header border-0">
+              <h3 class="card-title">
+                <i class="fas fa-map-marker-alt mr-1"></i>
+                Visitors
+              </h3>
+              <!-- card tools -->
+              <div class="card-tools">
+                <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+              </div>
+              <!-- /.card-tools -->
+            </div>
+            <div class="card-body">
+
+            </div>
+            <!-- /.card-body-->
+          </div>
+        </section>
+        <section class="col-lg-6 connectedSortable">
+          <div class="card bg-gradient-success">
+            <div class="card-header border-0">
+              <h3 class="card-title">
+                <i class="fas fa-map-marker-alt mr-1"></i>
+                Visitors
+              </h3>
+
+              <div class="card-tools">
+                <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+              </div>
+
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+          <div class="card bg-gradient-primary">
+            <div class="card-header border-0">
+              <h3 class="card-title">
+                <i class="fas fa-map-marker-alt mr-1"></i>
+                Visitors
+              </h3>
+              <!-- card tools -->
+              <div class="card-tools">
+                <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
+                  <i class="fas fa-minus"></i>
+                </button>
+              </div>
+              <!-- /.card-tools -->
+            </div>
+            <div class="card-body">
+
+            </div>
+            <!-- /.card-body-->
+          </div>
+        </section>
+      </div>
+    </div>
+  </section>
+
+</div>
+<?php
+include_once('Part/footer.html');
+?>
